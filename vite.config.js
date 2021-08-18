58da833b938ef4562fd108e962c8f4b2f7716dbe
kkorz <1291023564@qq.com>
2021-08-18 15:40:11 +0800

    chore: 添加postcss-autoprefixer支持

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
     open: true,
   },
   css: {
+    // PostCSS 配置（格式同 postcss.config.js）
+    // postcss-load-config 的插件配置
+    postcss: {
+      plugins: [require("autoprefixer")],
+    },
+    //指定传递给 CSS 预处理器的选项
     preprocessorOptions: {
       less: {
         // antd主题色
