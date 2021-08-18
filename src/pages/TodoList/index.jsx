d7ff1d911f07cd451070c6b35bbb4b3b7a47a42b
kkorz <1291023564@qq.com>
2021-08-18 11:11:59 +0800

    style: 美化todolist样式

--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -1,43 +1,96 @@
-import React from "react";
-import { observer, inject } from "mobx-react";
-import TodoItem from "./TodoItem";
+import React, { useState, useMemo } from "react";
+import { Input, Button, Space, Divider } from "antd";
+import { CheckCircleOutlined, DeleteOutlined } from "@ant-design/icons";
+import "./index.less";
 
-class TodoList extends React.Component {
-  state = {
-    inputRef: React.createRef(),
-  };
+const TodoList = () => {
+  const [inputVal, setInputVal] = useState("");
+  const [list, setList] = useState([]);
 
-  addItem = (e) => {
-    e.preventDefault();
-    const { inputRef } = this.state;
-    const { createItem } = this.props.todolist;
-
-    if (inputRef.current.value) {
-      createItem(inputRef.current.value);
-      inputRef.current.value = "";
-    } else {
-      alert("输入不能为空");
+  const addTask = () => {
+    if (inputVal) {
+      setList((list) => [
+        ...list,
+        {
+          name: inputVal,
+          done: false,
+          id: Date.now(),
+        },
+      ]);
+      setInputVal("");
     }
   };
 
-  render() {
-    const { inputRef } = this.state;
-    const { len } = this.props.todolist;
+  const onFinishTask = (id) => {
+    const index = list.findIndex((item) => item.id === id);
+    list[index].done = true;
+    setList([...list]);
+  };
 
-    return (
-      <>
-        <header>
-          <form onSubmit={this.addItem}>
-            <input type="text" placeholder="请输入待办事项" ref={inputRef} />
-          </form>
-        </header>
-        <section>
-          <TodoItem />
-        </section>
-        <footer> 未完成任务数 {len}</footer>
-      </>
-    );
-  }
-}
+  const onDeleleTask = (id) => {
+    const index = list.findIndex((item) => item.id === id);
+    list.splice(index, 1);
+    setList([...list]);
+  };
 
-export default inject("todolist")(observer(TodoList));
+  const [doneList, undoList] = useMemo(() => {
+    const finishedList = list.filter((item) => item.done);
+    const unFinishList = list.filter((item) => !item.done);
+    return [finishedList, unFinishList];
+  }, [list]);
+
+  return (
+    <div className="todolist">
+      <h2 className="todolist-title">Todo List</h2>
+
+      <section className="todolist-top">
+        <Space>
+          <Input
+            placeholder="Add a task here..."
+            onChange={(e) => {
+              setInputVal(e.target.value);
+            }}
+            onPressEnter={addTask}
+            value={inputVal}
+          />
+          <Button type="primary" onClick={addTask}>
+            Add Task
+          </Button>
+        </Space>
+      </section>
+
+      <section className="todolist-main">
+        <p className="todolist-main-title">Undo: {undoList.length}</p>
+        {undoList.map((item) => (
+          <div className="todolist-main-item" key={item.id}>
+            <p>{item.name}</p>
+            <span style={{ cursor: "pointer" }}>
+              <Space>
+                <CheckCircleOutlined onClick={() => onFinishTask(item.id)} />
+                <DeleteOutlined
+                  onClick={() => onDeleleTask(item.id)}
+                  style={{ color: "#f5222d" }}
+                />
+              </Space>
+            </span>
+          </div>
+        ))}
+      </section>
+
+      <Divider />
+
+      <p className="todolist-main-title">Done: {doneList.length}</p>
+      {doneList.map((item) => (
+        <div className="todolist-main-item" key={item.id}>
+          <p> {item.name}</p>
+          <DeleteOutlined
+            onClick={() => onDeleleTask(item.id)}
+            style={{ color: "#f5222d" }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
