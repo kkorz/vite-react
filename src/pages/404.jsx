f6238505a35a99dc365810148cdfffbc645112a5
kkorz <1291023564@qq.com>
2021-08-19 14:30:55 +0800

    style: 修复404页面显示问题

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import NotFound from "../assets/img/404.jpg";
 
 const Index = () => {
-  return <img src={NotFound} alt="" />;
+  return (
+    <img
+      src={NotFound}
+      alt=""
+      style={{ display: "block", width: "100%", height: "100%" }}
+    />
+  );
 };
 
 export default Index;
