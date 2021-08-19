96a0c9b22529514ddfee7c229691c5357ba7c3bb
kkorz <1291023564@qq.com>
2021-08-19 18:14:28 +0800

    style: 美化UseToggle页面

--- src/pages/Hooks/UseToggle/index.jsx
+++ src/pages/Hooks/UseToggle/index.jsx
@@ -1,17 +1,42 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { Button } from "antd";
 import { useToggle } from "@/core/hooks/useToggle";
+import classNames from "classnames";
+import styles from "./index.module.less";
 
 const Index = () => {
   const [toggled, { setToggle, setOnToggle, setUnToggle }] = useToggle();
   return (
-    <Fragment>
-      <p>toggle status is {String(toggled)}</p>
-      <button onClick={() => setToggle()}>toggle it</button>
-      <button onClick={() => setToggle(true)}>toggle true</button>
-      <button onClick={() => setToggle(false)}>toggle false</button>
-      <button onClick={() => setOnToggle()}>toggle true function</button>
-      <button onClick={() => setUnToggle()}>toggle false function</button>
-    </Fragment>
+    <div className={styles["toggle-wrapper"]}>
+      <section className={styles["toggle-btns"]}>
+        <Button onClick={() => setToggle()} type="primary">
+          toggle it
+        </Button>
+        <Button onClick={() => setToggle(true)} type="primary">
+          toggle true
+        </Button>
+        <Button onClick={() => setToggle(false)} type="primary">
+          toggle false
+        </Button>
+        <Button onClick={() => setOnToggle()} type="primary">
+          toggle true function
+        </Button>
+        <Button onClick={() => setUnToggle()} type="primary">
+          toggle false function
+        </Button>
+      </section>
+
+      <div
+        className={classNames(
+          styles["toggle-status"],
+          toggled
+            ? styles["toggle-status-true"]
+            : styles["toggle-status-false"],
+        )}
+      >
+        {String(toggled)}
+      </div>
+    </div>
   );
 };
 
