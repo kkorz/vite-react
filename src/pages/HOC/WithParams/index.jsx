579f87e819ba659995880aede9baa54005a737bc
kkorz <1291023564@qq.com>
2021-08-19 17:48:40 +0800

    style: 美化WithParams页面

--- src/pages/HOC/WithParams/index.jsx
+++ src/pages/HOC/WithParams/index.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import WithParams from "@/core/hoc/WithParams";
-import { Input, Button } from "antd";
+import { Input, Button, Divider, Space } from "antd";
 
 const Index = (props) => {
   const { params, history } = props;
@@ -17,12 +17,24 @@ const Index = (props) => {
 
   return (
     <div>
-      <h2>
-        URL的params是：
-        <span style={{ color: "red" }}>{params}</span>
-      </h2>
-      <Input ref={inputRef} style={{ width: 200 }} />
-      <Button onClick={changUrl}>点我改变url</Button>
+      <h2>WithParams</h2>
+      <h3>利用高阶函数（HOC），向目标组件注入一些新的属性，例如params</h3>
+
+      <Divider />
+      <h3>
+        当前页面的params是：
+        <span style={{ color: "blue" }}>{params}</span>
+      </h3>
+      <Space>
+        <Input
+          ref={inputRef}
+          style={{ width: 200 }}
+          placeholder="请输入参数name的值"
+        />
+        <Button onClick={changUrl} type="primary">
+          点我改变Url
+        </Button>
+      </Space>
     </div>
   );
 };
