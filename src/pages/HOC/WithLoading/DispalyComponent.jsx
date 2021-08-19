b8f21bb836adfd1f785b6a8f43d38a0d0eff4960
kkorz <1291023564@qq.com>
2021-08-19 17:28:14 +0800

    style: 美化withLoading页面

--- src/pages/HOC/WithLoading/DispalyComponent.jsx
+++ src/pages/HOC/WithLoading/DispalyComponent.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Card } from "antd";
 import WithLoading from "@/core/hoc/WithLoading";
 
 const DispalyComponent = () => {
   return (
-    <div style={{ padding: "10px", border: ".5px solid red" }}>
+    <Card>
       <p>我是展示组件</p>
       <p>被WithLoading包裹</p>
       <p>会根据传入的loading属性展示Loading状态</p>
-    </div>
+    </Card>
   );
 };
 
--- src/pages/HOC/WithLoading/index.jsx
+++ src/pages/HOC/WithLoading/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Divider } from "antd";
 import DispalyComponent from "./DispalyComponent";
 
 const Index = () => {
@@ -13,8 +14,17 @@ const Index = () => {
 
   return (
     <>
+      <h2>WithLoading</h2>
+      <h3>
+        利用高阶函数（HOC）进行<strong>渲染劫持</strong>
+        ，根据传入的loading状态决定展示Loading组件还是目标组件
+      </h3>
       <DispalyComponent loading={loading} />
-      <button onClick={fetchRemote}>点我请求数据</button>
+
+      <Divider />
+      <Button type="primary" onClick={fetchRemote}>
+        模拟请求接口
+      </Button>
     </>
   );
 };
