6e7b6aa1b7429c1fb8e453cc1d95b0805b100329
kkorz <1291023564@qq.com>
2021-08-24 11:11:52 +0800

    chore: 新增json-server

---
+++ src/core/hooks/index.js
@@ -0,0 +1,21 @@
+import { useDebounce } from "./useDebounce";
+import { useDebounceFn } from "./useDebounceFn";
+import { useMount } from "./useMount";
+import { useSet } from "./useSet";
+import { useThrottle } from "./useThrottle";
+import { useThrottleFn } from "./useThrottleFn";
+import { useTitle } from "./useTitle";
+import { useToggle } from "./useToggle";
+import { useUnmount } from "./useUnmount";
+
+export {
+  useDebounce,
+  useDebounceFn,
+  useMount,
+  useSet,
+  useThrottle,
+  useThrottleFn,
+  useTitle,
+  useToggle,
+  useUnmount,
+};
