a6a4a15b82cbd6194a8eb3f2f1f766cf69e14f75
zhaokang <1291023564@qq.com>
2021-09-15 18:04:45 +0800

    chore: 打包自动复制dist文件到vite-react-page

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const fse = require("fs-extra");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 打包后抽离 css 文件
 const OptimizeCssPlugin = require("optimize-css-assets-webpack-plugin"); // css压缩
+const FileManagerPlugin = require("filemanager-webpack-plugin"); // 操作打包文件
 const { themeVariables } = require("./src/config/theme");
+
+const appDirectory = fse.realpathSync(process.cwd()); // 当前项目根目录
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+const targetDir = resolveApp("../vite-react-page"); // target目录，此处是vite-react-page项目根目录
 
 module.exports = {
   mode: "production",
@@ -55,5 +61,12 @@ module.exports = {
       filename: "css/[name].css",
     }),
     new OptimizeCssPlugin(),
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [{ source: "./dist", destination: targetDir }],
+        },
+      },
+    }),
   ],
 };
